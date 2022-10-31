3f70de1ca94b6c2fea7571185f03a4dab251bbbf
dvyadav <divyanshuy858@gmail.com>
2022-10-31 07:34:06 +0530

    sucess

--- deletion/delete.c
+++ deletion/delete.c
@@ -7,17 +7,20 @@ int main()
     scanf_s("%d",&n);
     for(int i=0;i<n;i++)
     {
-        printf("\nenter the data num %d",i+1);
+        printf("\nenter the data num %d= ",i+1);
         scanf_s("%d",&arr[i]);
     }
     printf("enter the location of the deletion data:");
     scanf_s("%d",&loc);
     deletion(arr,n,loc);
+    for(int i=0;i<n-1;i++)
+    printf("\n %d",arr[i]);
 }
 int deletion(int arr[],int n,int loc)
 {
-    for(int i=0;i<n;i++)
+   
+    for(int i=loc;i<n;i++)
     {
-        printf("\n %d",arr[i]);
+         arr[i-1]=arr[i];
     }
 }
