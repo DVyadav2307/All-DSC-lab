e36f9fa98950ee801da67d7febb376c1a817e3ea
dvyadav <divyanshuy858@gmail.com>
2022-10-31 07:20:15 +0530

    checking

--- deletion/delete.c
+++ deletion/delete.c
@@ -1,1 +1,23 @@
-git
+#include<stdio.h>
+int deletion();
+int main()
+{
+    int arr[100],n,loc;
+    printf("enter the lenght of the dtat:");
+    scanf_s("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        printf("\nenter the data num %d",i+1);
+        scanf_s("%d",&arr[i]);
+    }
+    printf("enter the location of the deletion data:");
+    scanf_s("%d",&loc);
+    deletion(arr,n,loc);
+}
+int deletion(int arr[],int n,int loc)
+{
+    for(int i=0;i<n;i++)
+    {
+        printf("\n %d",arr[i]);
+    }
+}
