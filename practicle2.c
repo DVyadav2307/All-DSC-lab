cbcf84598b8f892e950bd6e50866194b667e1140
DVyadav2307 <divyanshuy858@gmail.com>
2022-11-21 11:11:16 +0530

    completed from vs code

--- practicle2.c
+++ practicle2.c
@@ -9,19 +9,30 @@ please appreciate*/
 #include<stdlib.h>
 int main()
 {
-	int c1_elem, c2_elem, r1_elem, r2_elem,m[10][10],n[10][10];
+	int c1_elem,  r1_elem, m[10][10],n[10][10],x[10][10];
 	printf("\nEnter the  the no. of rows and columns <with space> of matrix-1");
 	scanf_s("%d%d", &c1_elem, &r1_elem);
-	printf("\nEnter the  the no. of rows and columns <with space> of matrix-2");
-	scanf_s("%d%d", &c2_elem, &r2_elem);
+	
 	printf("\nEnter the matrix-1 carefully\n");
+	//READING MATRIX 1
 	for (int i = 0; i < c1_elem; i++)
-		for (int j = 0; j < r1_elem; j++)
+	{	for (int j = 0; j < r1_elem; j++)
 			scanf_s("%d", &m[j][i]);
+	}
+	//READING MATRIX 2
+	printf("\nEnter the matrix-2 carefully\n");
+	for (int i = 0; i < c1_elem; i++)
+	{	for (int j = 0; j < r1_elem; j++)
+		{	scanf_s("%d", &n[j][i]);
+			x[j][i]=m[j][i]+n[j][i];
+		}
+	}
+	//printing matrix
+	printf("\nthe additionj of maxtrix is follows-");
 	for (int i = 0; i < c1_elem; i++)
 	{
 		printf("\n");
 		for (int j = 0; j < r1_elem; j++)
-			printf("\t%d", m[j][i]);
+			printf("\t%d", x[j][i]);
 	}
 }
